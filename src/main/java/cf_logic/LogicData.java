d3988cdbb08f498ef5ec71ec952bc86adbb730fd
Anirudh Parui <anirudh.parui@capitalfloat.com>
2016-11-10 14:35:54 +0530

    Async flow

---
+++ src/main/java/cf_logic/LogicData.java
@@ -0,0 +1,77 @@
+package cf_logic;
+
+public class LogicData {
+    Integer plan_id;
+    String product_id;
+    Integer step_num;
+    Boolean proceed;
+    Integer consumerId;
+    Boolean claimed;
+    Data data;
+
+    public Integer getPlan_id() {
+        return plan_id;
+    }
+
+    public void setPlan_id(Integer plan_id) {
+        this.plan_id = plan_id;
+    }
+
+    public String getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(String product_id) {
+        this.product_id = product_id;
+    }
+
+    public Integer getStep_num() {
+        return step_num;
+    }
+
+    public void setStep_num(Integer step_num) {
+        this.step_num = step_num;
+    }
+    public Boolean getProceed() {
+        return proceed;
+    }
+
+    public void setProceed(Boolean proceed) {
+        this.proceed = proceed;
+    }
+
+    public Integer getConsumerId() {
+        return consumerId;
+    }
+
+    public void setConsumerId(Integer consumerId) {
+        this.consumerId = consumerId;
+    }
+    public Boolean getClaimed() {
+        return claimed;
+    }
+
+    public void setClaimed(Boolean claimed) {
+        this.claimed = claimed;
+    }
+
+    public Data getData() {
+        return data;
+    }
+
+    public void setData(Data data) {
+        this.data = data;
+    }
+    public LogicData(Integer plan_id, String product_id, Integer step_num,Boolean proceed, Data data) {
+        this.plan_id = plan_id;
+        this.product_id = product_id;
+        this.step_num = step_num;
+        this.proceed = proceed;
+        this.consumerId = 125;
+        this.claimed = false;
+        this.data = data;
+    }
+
+    public LogicData() {
+    }
+}
--- src/main/java/jm_logic/Main.java
+++ src/main/java/jm_logic/Main.java
@@ -2,6 +2,12 @@ package jm_logic;
 
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.Context;
+import com.google.gson.Gson;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.io.DataOutputStream;
+import java.io.IOException;
 
 public class Main implements RequestHandler<Request, Response> {
 
@@ -12,6 +18,36 @@ public class Main implements RequestHandler<Request, Response> {
         if( Integer.parseInt(request.data.last_six_months_average) > 150000 && Integer.parseInt(request.data.applicant_pan) > 600) {
             proceed = true;
         }
-        return new Response(request.plan_id,request.product_id,request.step_num,proceed,request.data);
+        LogicData logic_data = new LogicData(request.plan_id,request.product_id,request.step_num,proceed,request.data);
+        Gson gson=new Gson();
+        String post_data = gson.toJson(logic_data);
+        
+        try {
+            String url = "http://thebox.capitalfloat.io/thebox/plan/consumer_response.json";
+            URL obj = new URL(url);
+
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+            //add reuqest header
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Authorization", "fa0a619df9667fbd3b691e9feff52b80c3a399ad9139b2ad61f0ff1f");
+            con.setRequestProperty("Content-Type", "application/json");
+            con.setDoOutput(true);
+            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+            wr.writeBytes(post_data);
+            wr.flush();
+            wr.close();
+
+            int responseCode = con.getResponseCode();
+            System.out.println("\nSending 'POST' request to URL : " + url);
+            System.out.println("Post parameters : " + post_data);
+            System.out.println("Response Code : " + responseCode);
+        } catch (MalformedURLException e){
+            System.out.println(e);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        return new Response();
     }
 }
